309e225a39d91fda34557002dea452790f318a80
Danar Hikmah Krismanto <hikmahdanar@gmail.com>
2021-05-13 17:42:18 +0700

    Initial Commit

---
+++ MyMonee-MoneySaverTests/MyMonee_MoneySaverTests.swift
@@ -0,0 +1,33 @@
+//
+//  MyMonee_MoneySaverTests.swift
+//  MyMonee-MoneySaverTests
+//
+//  Created by MacBook on 13/05/21.
+//
+
+import XCTest
+@testable import MyMonee_MoneySaver
+
+class MyMonee_MoneySaverTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
